9c8a9ca846f801e6caad91221ff201cb7fdbdfa2
bartv <bart.veldstra@engine-software.nl>
2014-12-06 23:21:42 +0100

    DisplacedMarker follows player around

---
+++ Assets/Follower.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+
+    public Transform target;
+
+    void FixedUpdate () {
+        rigidbody.MovePosition(target.position);
+    }
+}
