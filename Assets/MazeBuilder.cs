546c3202571bf5531673c7e1ed2149902e2b2558
bartv <bart.veldstra@engine-software.nl>
2014-12-06 12:29:19 +0100

    Surround arena with real walls

--- Assets/MazeBuilder.cs
+++ Assets/MazeBuilder.cs
@@ -11,6 +11,7 @@ public class MazeBuilder : MonoBehaviour {
 
     public GameObject EdgePillar;
     public GameObject Floor;
+    public GameObject Wall;
     public GameObject AugmentedWall;
 
     void Start () {
@@ -18,18 +19,30 @@ public class MazeBuilder : MonoBehaviour {
         EdgePillar.transform.parent = transform;
         AugmentedWall.SetActive(false);
         AugmentedWall.transform.parent = transform;
+        Wall.SetActive(false);
+        Wall.transform.parent = transform;
         Floor.transform.parent = transform;
 
         GenerateFrame();
     }
 
-    void PlaceWall(Vector3 pos, int angle, string name)
+    void PlaceTemplate(GameObject obj, Vector3 pos, int angle, string name)
     {
-        var wall = GameObject.Instantiate(AugmentedWall) as GameObject;
+        var wall = GameObject.Instantiate(obj) as GameObject;
         wall.SetActive(true);
         wall.name = name;
         wall.transform.localRotation = Quaternion.Euler(0, angle, 0);
         wall.transform.localPosition = pos;
+    }
+
+    void PlaceWall(Vector3 pos, int angle, string name)
+    {
+        PlaceTemplate(Wall, pos, angle, name);
+    }
+
+    void PlaceAugmentedWall(Vector3 pos, int angle, string name)
+    {
+        PlaceTemplate(AugmentedWall, pos, angle, name);
     }
 
     void GenerateFrame()
@@ -42,29 +55,29 @@ public class MazeBuilder : MonoBehaviour {
             for (int row = 0; row < MazeHeight + 1; ++row)
             {
                 var pos = down * row + right * col;
-                var pillar = GameObject.Instantiate(EdgePillar) as GameObject;
-                pillar.SetActive(true);
-                pillar.name = string.Format("Pillar_{0:D2}_{1:D2}", col, row);
-                pillar.transform.localPosition = pos;
+                var name = string.Format("Pillar_{0:D2}_{1:D2}", col, row);
+                PlaceTemplate(EdgePillar, pos, 0, name);
             }
         }
 
         for (int row = 0; row < MazeHeight; ++row)
         {
-            var posX = down * row + down * 0.5f;
-            var nameA = string.Format("AugmentedBound_L{0:D2}", row);
-            PlaceWall(posX, 270, nameA);
-            var nameB = string.Format("AugmentedBound_R{0:D2}", row);
-            PlaceWall(posX + right * MazeWidth, 90, nameB);
+            var posL = down * row + down * 0.5f;
+            var posR = posL + right * MazeWidth;
+            PlaceAugmentedWall(posL, 270, string.Format("AugmentedBound_L{0:D2}", row));
+            PlaceAugmentedWall(posR, 90, string.Format("AugmentedBound_R{0:D2}", row));
+            PlaceWall(posL, 270, string.Format("Bound_L{0:D2}", row));
+            PlaceWall(posR, 90, string.Format("Bound_R{0:D2}", row));
         }
 
         for (int col = 0; col < MazeWidth; ++col)
         {
-            var posY = right * col + right * 0.5f;
-            var nameA = string.Format("AugmentedBound_B{0:D2}", col);
-            PlaceWall(posY, 180, nameA);
-            var nameB = string.Format("AugmentedBound_T{0:D2}", col);
-            PlaceWall(posY + down * MazeHeight, 0, nameB);
+            var posB = right * col + right * 0.5f;
+            var posT = posB + down * MazeHeight;
+            PlaceAugmentedWall(posB, 180, string.Format("AugmentedBound_B{0:D2}", col));
+            PlaceAugmentedWall(posT, 0, string.Format("AugmentedBound_T{0:D2}", col));
+            PlaceWall(posB, 180, string.Format("Bound_B{0:D2}", col));
+            PlaceWall(posT, 0, string.Format("Bound_T{0:D2}", col));
         }
 
         Floor.transform.localPosition = right * -0.5f + down * -0.5f;
