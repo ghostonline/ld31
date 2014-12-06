2d740df50d1eeb6198649b79d6c3ec7f5afc342b
bartv <bart.veldstra@engine-software.nl>
2014-12-06 13:40:45 +0100

    Phone can be held more closely to aim

---
+++ Assets/PhoneController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneController : MonoBehaviour {
+
+    public Transform phone;
+    public Transform normal;
+    public Transform aim;
+    
+    public int aimButton = 0;
+
+    bool aiming;
+
+    void AlignPhone(Transform anchor)
+    {
+        phone.parent = anchor;
+        phone.localPosition = Vector3.zero;
+        phone.localRotation = Quaternion.identity;
+    }
+
+    void Start () {
+        AlignPhone(normal);
+    }
+
+    void Update () {
+        if (Input.GetMouseButton(aimButton) && !aiming)
+        {
+            AlignPhone(aim);
+            aiming = true;
+        }
+        else if (!Input.GetMouseButton(aimButton) && aiming)
+        {
+            AlignPhone(normal);
+            aiming = false;
+        }
+    }
+}
