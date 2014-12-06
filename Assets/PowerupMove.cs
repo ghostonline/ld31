33dfa0a76ff28a3d30e5362809892c05a1bbff67
bartv <bart.veldstra@engine-software.nl>
2014-12-06 14:06:38 +0100

    Added marker

---
+++ Assets/PowerupMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupMove : MonoBehaviour {
+
+    public Transform mesh;
+    public float turnSpeed = 80f;
+    public float bounceSpeed = 5f;
+    public float bounceHeight = 0.1f;
+
+    float bounceProgress;
+    Vector3 baseHeight;
+
+    void Start()
+    {
+        baseHeight = mesh.localPosition;
+    }
+
+    void Update ()
+    {
+        mesh.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        
+        bounceProgress += Time.deltaTime * bounceSpeed;
+        if (bounceProgress > 2 * Mathf.PI)
+        {
+            bounceProgress -= 2 * Mathf.PI;
+        }
+
+        mesh.localPosition = baseHeight + Vector3.up * bounceHeight * Mathf.Sin(bounceProgress);
+    }
+}
