4a38210a88c52c355df1a4a8d2fd0711108e47e0
bartv <bart.veldstra@engine-software.nl>
2014-12-07 16:19:13 +0100

    Keys can be collected

---
+++ Assets/KeyCounter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyCounter : MonoBehaviour {
+
+    public int keys;
+
+    void OnPickup(GameObject key)
+    {
+        ++keys;
+        Destroy(key);
+    }
+
+}
---
+++ Assets/Pickup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+    public GameObject PickupListener;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(string.Format("Pickup {0}", name));
+        PickupListener.SendMessage("OnPickup", gameObject);
+    }
+}
