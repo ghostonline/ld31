d54ff6465ac6db01e2af897d7f510d308b24433c
bartv <bart.veldstra@engine-software.nl>
2014-12-07 17:23:08 +0100

    Pick up end level marker after obtaining 3 keys

---
+++ Assets/EndLevelTracker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevelTracker : MonoBehaviour {
+
+    public KeyCounter counter;
+    public int minKeyCount;
+
+    void OnPickup(GameObject obj)
+    {
+        if (counter.keys < minKeyCount)
+        {
+            Debug.Log("Need more keys");
+        }
+        else
+        {
+            Destroy(obj);
+            Debug.Log("Game complete!");
+        }
+    }
+}
