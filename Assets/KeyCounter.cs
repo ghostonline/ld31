e55f47a9b315d72a9f671c2ef75aae8822467603
bartv <bart.veldstra@engine-software.nl>
2014-12-07 16:30:51 +0100

    Number of keys collected are represented in HUD

--- Assets/KeyCounter.cs
+++ Assets/KeyCounter.cs
@@ -5,8 +5,23 @@ public class KeyCounter : MonoBehaviour {
 
     public int keys;
 
+    public GameObject[] keyIcons;
+
+    void Start()
+    {
+        foreach (var icon in keyIcons)
+        {
+            if (icon != null) { icon.SetActive(false); }
+        }
+    }
+
     void OnPickup(GameObject key)
     {
+        if (keys < keyIcons.Length && keyIcons[keys] != null)
+        {
+            keyIcons[keys].SetActive(true);
+        }
+
         ++keys;
         Destroy(key);
     }
