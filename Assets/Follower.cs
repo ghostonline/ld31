56c1eacc1a416570174c5cfdb34043a732a0fccd
bartv <bart.veldstra@engine-software.nl>
2014-12-07 10:19:21 +0100

    Follower disconnects when distance is too large

--- Assets/Follower.cs
+++ Assets/Follower.cs
@@ -5,9 +5,18 @@ public class Follower : MonoBehaviour {
 
     public Transform target;
     public float speed = 10.0f;
+    public float disconnectDistance = 0.5f;
 
     void FixedUpdate () {
-        var nearest = Vector3.MoveTowards(transform.position, target.position, 1);
-        rigidbody.velocity = (nearest - transform.position) * speed;
+        var dist = Vector3.Distance(transform.position, target.position);
+        if (dist > disconnectDistance)
+        {
+            rigidbody.velocity = Vector3.zero;
+        }
+        else
+        {
+            var nearest = Vector3.MoveTowards(transform.position, target.position, 1);
+            rigidbody.velocity = (nearest - transform.position) * speed;
+        }
     }
 }
