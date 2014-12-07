39ab1b7ea0be4a55af87a266e5665c3e853387c9
bartv <bart.veldstra@engine-software.nl>
2014-12-08 00:06:28 +0100

    Detect taps on the phone

--- Assets/Scripts/PhoneController.cs
+++ Assets/Scripts/PhoneController.cs
@@ -3,19 +3,31 @@ using System.Collections;
 
 public class PhoneController : MonoBehaviour {
 
+    public Camera viewCamera;
+    public Collider screen;
     public Transform phone;
     public Transform normal;
     public Transform aim;
     
     public int aimButton = 0;
+    public int selectButton = 1;
 
     bool aiming;
+    bool hot;
+    bool selectDown;
 
     void AlignPhone(Transform anchor)
     {
         phone.parent = anchor;
         phone.localPosition = Vector3.zero;
         phone.localRotation = Quaternion.identity;
+    }
+
+    bool IsOnScreen()
+    {
+        var ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        return screen.Raycast(ray, out hit, float.PositiveInfinity);
     }
 
     void Start () {
@@ -32,6 +44,24 @@ public class PhoneController : MonoBehaviour {
         {
             AlignPhone(normal);
             aiming = false;
+        }
+
+        if (Input.GetMouseButton(selectButton))
+        {
+            if (!selectDown)
+            {
+                hot = IsOnScreen();
+                selectDown = true;
+            }
+        }
+        else if (selectDown)
+        {
+            if (hot && IsOnScreen())
+            {
+                Debug.Log("Screen tap");
+            }
+            hot = false;
+            selectDown = false;
         }
     }
 }
