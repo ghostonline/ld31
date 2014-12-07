14fe494dd0adc5a1a695f82e3843ab780cac8177
bartv <bart.veldstra@engine-software.nl>
2014-12-07 01:46:32 +0100

    Show pointer when displaced

---
+++ Assets/DisplacementPointer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplacementPointer : MonoBehaviour
+{
+
+    public Transform rotationMirror;
+    public GameObject geometry;
+    public DistanceTrigger trigger;
+
+    bool visible = true;
+
+    void Update () {
+        if (trigger.shouldTrigger)
+        {
+            transform.rotation = Quaternion.Inverse(rotationMirror.rotation);
+        }
+
+        if (trigger.shouldTrigger != visible)
+        {
+            visible = trigger.shouldTrigger;
+            geometry.SetActive(visible);
+        }
+    }
+}
