8ac89b860fa5f64504e114a67dd38464956d7c7b
bartv <bart.veldstra@engine-software.nl>
2014-12-07 01:10:32 +0100

    Created distance trigger component

---
+++ Assets/DistanceTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceTrigger : MonoBehaviour {
+
+    public Transform target;
+    public float maxDistance = 0.5f;
+
+    public bool shouldTrigger;
+    public float currentDistance;
+    
+    void FixedUpdate () {
+        currentDistance = Vector3.Distance(target.position, transform.position);
+        shouldTrigger = currentDistance > maxDistance;
+    }
+}
