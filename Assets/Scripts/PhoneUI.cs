b58d4cc78024e8120702773dcade36ef3595af8d
bartv <bart.veldstra@engine-software.nl>
2014-12-08 00:51:26 +0100

    Allow showing signs in UI

--- Assets/Scripts/PhoneUI.cs
+++ Assets/Scripts/PhoneUI.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class PhoneUI : MonoBehaviour {
-    
+
+    enum Screen
+    {
+        Sign,
+        Victory,
+        InGame,
+    }
+
     public GameObject SignPanel;
     public GameObject VictoryPanel;
     public GameObject InGamePanel;
@@ -11,14 +19,59 @@ public class PhoneUI : MonoBehaviour {
     // 3D Game UI components
     public GameObject GameUI3DRoot;
 
-    public void Start()
+    public Text SignText;
+
+    List<string> signs;
+    Screen current;
+
+    void Awake()
     {
-        SignPanel.SetActive(false);
-        VictoryPanel.SetActive(false);
+        signs = new List<string>();
+        current = Screen.InGame;
+    }
+    
+    void Start()
+    {
+        PushSign("Hello!");
+    }
+
+    void ShowScreen(Screen screen)
+    {
+        SignPanel.SetActive(screen == Screen.Sign);
+        VictoryPanel.SetActive(screen == Screen.Victory);
+        InGamePanel.SetActive(screen == Screen.InGame);
+        GameUI3DRoot.SetActive(screen == Screen.InGame);
+        current = screen;
+
+        if (signs.Count > 0) { SignText.text = signs[0]; }
     }
 
     public void OnScreenTap()
     {
-        Debug.Log("Screen tapped");
+        if (current == Screen.Sign)
+        {
+            signs.RemoveAt(0);
+            if (signs.Count > 0)
+            {
+                ShowScreen(Screen.Sign);
+            }
+            else
+            {
+                ShowScreen(Screen.InGame);
+            }
+        }
+    }
+
+    public void PushSign(string text)
+    {
+        if (!signs.Contains(text))
+        {
+            signs.Add(text);
+        }
+
+        if (current != Screen.Sign)
+        {
+            ShowScreen(Screen.Sign);
+        }
     }
 }
