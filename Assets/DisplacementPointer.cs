6a129eaa8cd58ac4e56535666ca2d63a70326b59
bartv <bart.veldstra@engine-software.nl>
2014-12-07 10:28:24 +0100

    Disable augmented layer when losing marker

--- Assets/DisplacementPointer.cs
+++ Assets/DisplacementPointer.cs
@@ -7,8 +7,15 @@ public class DisplacementPointer : MonoBehaviour
     public Transform rotationMirror;
     public GameObject geometry;
     public DistanceTrigger trigger;
+    public Camera PhoneCamera;
 
     bool visible = true;
+    int augmentedMask;
+
+    void Start()
+    {
+        augmentedMask = 1 << LayerMask.NameToLayer("Augmented");
+    }
 
     void Update () {
         if (trigger.shouldTrigger)
@@ -20,6 +27,15 @@ public class DisplacementPointer : MonoBehaviour
         {
             visible = trigger.shouldTrigger;
             geometry.SetActive(visible);
+
+            if (visible)
+            {
+                PhoneCamera.cullingMask &= ~augmentedMask;
+            }
+            else
+            {
+                PhoneCamera.cullingMask |= augmentedMask;
+            }
         }
     }
 }
